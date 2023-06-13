e15932d5d5d3c60e616b4c4c7e7c1fea52179a67
Simon <sstur@me.com>
2023-06-13 07:06:24 -0700

    Move signup route to users.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,4 @@
 export { default as auth } from './auth';
 export { default as main } from './main';
 export { default as posts } from './posts';
-export { default as signup } from './signup';
 export { default as users } from './users';
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,17 @@
+import { HttpError } from '@nbit/express';
+
 import { defineRoutes } from '../server';
 import { db } from '../db';
+import { schema } from '../support/schema';
+
+const UserCreateInput = schema(({ Record, String }) => {
+  return Record({
+    name: String,
+    profilePhoto: String.optional(),
+    username: String,
+    password: String,
+  });
+});
 
 export default defineRoutes((app) => [
   app.get('/users', async () => {
@@ -10,5 +22,39 @@ export default defineRoutes((app) => [
     const { id } = request.params;
     const user = await db.User.getById(id);
     return user ?? undefined;
+  }),
+
+  app.post('/signup', async (request) => {
+    const body = await request.json();
+    if (!UserCreateInput.guard(body)) {
+      throw new HttpError({ status: 400 });
+    }
+    const { name, profilePhoto, username, password } = body;
+    if (!username.length || username.match(/\W/)) {
+      throw new HttpError({ status: 400, message: 'Invalid username' });
+    }
+    const existingUsers = await db.User.findWhere(
+      (user) => user.username.toLowerCase() === username.toLowerCase(),
+    );
+    if (existingUsers.length) {
+      throw new HttpError({ status: 400, message: 'Username already exists' });
+    }
+    const user = await db.User.insert({
+      name,
+      profilePhoto: profilePhoto ?? '',
+      username,
+      password,
+    });
+    const now = new Date().toISOString();
+    const session = await db.Session.insert({ user: user.id, createdAt: now });
+    return {
+      success: true,
+      user: {
+        id: user.id,
+        name,
+        username,
+      },
+      token: session.id,
+    };
   }),
 ]);
