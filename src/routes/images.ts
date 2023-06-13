50b13e3af31f623fa778d0d1c01db90b5de21841
Simon <sstur@me.com>
2023-06-13 11:52:54 -0700

    Implement uploading to image server

--- src/routes/images.ts
+++ src/routes/images.ts
@@ -2,6 +2,7 @@ import fs from 'fs/promises';
 import { createReadStream, createWriteStream } from 'fs';
 import { resolve } from 'path';
 
+import fetch from 'node-fetch';
 import { HttpError, Response } from '@nbit/express';
 
 import { defineRoutes } from '../server';
@@ -14,9 +15,19 @@ import { pipeStreamAsync } from '../support/pipeStreamAsync';
 import { sign } from '../support/signature';
 import { createId } from '../support/createId';
 import { createTimestamp } from '../support/timestamp';
+import { IMAGE_UPLOAD_URL, SECRET_KEY } from '../support/constants';
+import { schema } from '../support/schema';
 
 const uploadsDirRelative = '../../uploads';
 const uploadsDir = resolve(__dirname, uploadsDirRelative);
+
+const UploadResponseBody = schema(({ Record, String }) => {
+  return Record({
+    id: String,
+    fileName: String,
+    url: String,
+  });
+});
 
 export default defineRoutes((app) => [
   app.get('/images/:fileName', async (request) => {
@@ -45,6 +56,33 @@ export default defineRoutes((app) => [
     if (!imageType) {
       throw new HttpError({ status: 400 });
     }
+
+    if (IMAGE_UPLOAD_URL) {
+      const response = await fetch(IMAGE_UPLOAD_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/octet-stream',
+          'Content-Disposition': contentType,
+          Authorization: `Bearer ${SECRET_KEY}`,
+        },
+        body: request.body,
+      });
+      if (!response.ok) {
+        throw new HttpError({
+          status: 500,
+          message: `Unexpected response status from upstream server: ${response.status}`,
+        });
+      }
+      const result = await response.json();
+      if (!UploadResponseBody.guard(result)) {
+        throw new HttpError({
+          status: 500,
+          message: `Unexpected response body from upstream server`,
+        });
+      }
+      return result;
+    }
+
     const id = sign(createTimestamp() + createId() + imageType.id);
     await fs.mkdir(uploadsDir, { recursive: true });
     const fileName = id + '.' + imageType.ext;
